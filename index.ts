c234cbb54001b3c5a7c6c14d1e26ff3b86817a16
kasrakhosravi <kasrakhosravi2@gmail.com>
2024-04-02 03:25:59 +0200

    feat: prepareManifest

---
+++ index.ts
@@ -0,0 +1,64 @@
+import {
+    ProcessorType,
+    SolutionDefinition,
+    SolutionScriptFunction,
+} from 'flair-sdk'
+
+const PACKAGE_NAME = '@flair-sdk/basic-events-txs'
+
+export type Config = {
+    events?: {
+        enabled?: boolean
+    }
+    transactions?: {
+        enabled?: boolean
+    }
+}
+
+const definition: SolutionDefinition<Config> = {
+    prepareManifest: async (context, config, manifest) => {
+
+        if (config.events?.enabled) {
+            manifest.processors = [
+                ...(manifest.processors || []),
+                {
+                    id: 'basic-events',
+                    type: ProcessorType.Event,
+                    handler: `${PACKAGE_NAME}/src/processors/basic-events/handler.ts`,
+                    abi: `${PACKAGE_NAME}/src/abis/*.json`,
+                },
+            ]
+        }
+
+        if (config.transactions?.enabled) {
+            manifest.processors = [
+                ...(manifest.processors || []),
+                {
+                    id: 'basic-transactions',
+                    type: ProcessorType.Transaction,
+                    handler: `${PACKAGE_NAME}/src/processors/basic-transactions/handler.ts`,
+                    abi: `${PACKAGE_NAME}/src/abis/*.json`,
+                },
+            ]
+        }
+
+        manifest.processors = [
+                ...(manifest.processors || []),
+                {
+                    id: 'reorg',
+                    type: ProcessorType.Reorg,
+                    handler: `${PACKAGE_NAME}/src/processors/reorg/handler.ts`,
+                },
+            ]
+
+        return manifest
+    },
+    registerScripts: (
+        context,
+        config,
+    ): Record<string, SolutionScriptFunction> => {
+        return {}
+    },
+}
+
+export default definition
--- src/types.ts
+++
@@ -1,5 +0,0 @@
-export type DatabaseSyncEnricherParameters = {
-  // Relevant only within SQL query (for interpolation):
-  fromTimestamp?: string
-  toTimestamp?: string
-}
