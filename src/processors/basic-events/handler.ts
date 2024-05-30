94bb0b8a2147cbee1464692d58053ccc85c14847
kasrakhosravi <kasrakhosravi2@gmail.com>
2024-05-30 07:56:25 +0200

    fix: refactor event processor

--- src/processors/basic-events/handler.ts
+++ src/processors/basic-events/handler.ts
@@ -2,52 +2,54 @@ import { database } from "flair-sdk";
 // OPTIONAL: import { blockchain } from 'flair-sdk';
 import { EventHandlerInput } from "flair-sdk";
 
-export const processEvent = async (event: EventHandlerInput) => {
-  // OPTIONAL: You can fetch full transaction data if you need it.
-  //
-  // const provider = await blockchain.getProvider(event.chainId);
-  // const transaction = await provider
-  //   .cached()
-  //   .getTransactionReceipt(event.txHash);
 
-  await database.upsert({
-    // Entity type is used to group entities together.
-    // Here we're creating 1 entity for all events, but you can also create 1 per event.
-    entityType: "Event",
+export const processEvent = async (input: EventHandlerInput) => {
+  const eventName = input.parsed?.name || input.abi?.eventName
 
+  if (!eventName) {
+    throw new Error(
+      `Event name not found for log index ${input.log.logIndex} of ${input.txHash}`,
+    )
+  }
+
+  const data = {
     // Unique ID for this entity.
     //
     // Some useful tips:
     // - chainId makes sure if potentially same tx has happened on different chains it will be stored separately.
     // - hash and logIndex make sure this event is stored uniquely.
-    entityId: `${event.chainId}-${event.txHash}-${event.log.logIndex}`,
+    // - hash also makes sure with potential reorgs we don't store same event twice.
+    // - logIndex also makes sure if such event has happened multiple times in same tx it will be stored separately.
+    entityId: `${input.chainId}-${input.txHash}-${input.log.logIndex}`,
 
-    // Horizon helps with chronological ordering of events and handling re-orgs.
-    // This object will automatically create blockNumber and logIndex fields on your entity,
-    // so no need to add them separately.
-    horizon: event.horizon,
+    // Horizon helps with chronological ordering of events and handling re-orgs
+    // This will automatically add blockNumber and logIndex fields to the entity.
+    horizon: input.horizon,
 
     // You can store any data you want, even every single entity of the same type can have different fields.
-    // Must not include "entityId" field as it's already defined above.
-    chainId: event.chainId,
-    contractAddress: event.log.address,
-    blockTimestamp: event.blockTimestamp,
+    chainId: input.chainId,
+    contractAddress: input.log.address,
+    blockTimestamp: input.blockTimestamp,
+    txHash: input.txHash,
+  }
 
-    // OPTIONAL: Uncomment if you need actual EOA that sent this tx.
-    //
-    // txFrom: transaction.from,
-    // txTo: transaction.to,
-    txHash: event.txHash,
+  // All event args as-is
+  const eventArgs = {
+    ...input.parsed?.args,
+  }
 
-    eventName: event.parsed?.name || "UnknownEvent",
+  // 1) Store the event in a separate entity type
+  await database.upsert({
+    entityType: eventName,
+    ...data,
+    ...eventArgs,
+  })
 
-    // Save all event args as-is (and for unknown events store raw topics and data)
-    ...(event.parsed?.args || {
-      topic0: event.log.topics?.[0],
-      topic1: event.log.topics?.[1],
-      topic2: event.log.topics?.[2],
-      topic3: event.log.topics?.[3],
-      data: event.log.data,
-    }),
-  });
-};
+  // 2) Also store the event in a generic entity type
+  await database.upsert({
+    entityType: 'Event',
+    eventName,
+    ...data,
+    args: eventArgs,
+  })
+}
